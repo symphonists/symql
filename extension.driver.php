8e53c63d05ed813e21ee28770f78d3271069f799
Nick Dunn <nick@nick-dunn.co.uk>
2012-03-14 23:08:52 +0000

    Removed about() for 2.3

--- extension.driver.php
+++ extension.driver.php
@@ -1,15 +1,5 @@
 <?php
 
 Class Extension_SymQL extends Extension {
-
-	public function about(){
-		return array('name' => 'SymQL',
-					 'version' => '0.6.1',
-					 'release-date' => '2011-02-16',
-					 'author' => array('name' => 'Nick Dunn',
-									   'website' => 'http://nick-dunn.co.uk'),
-					'description' => 'An SQL-like syntax for querying entries from Symphony CMS'
-				);
-
-	}
+	
 }
